6191311e8e6a9080d569179efeeeb01751203aec
adriaensen.alexi <adriaensen.alexi@provilion.be>
2025-10-21 13:43:46 +0200

    vierkant my turtle

---
+++ turtle_klassen.py
@@ -0,0 +1,17 @@
+import turtle
+import random
+
+scherm = turtle.Screen()
+
+pen = turtle.Turtle()
+
+#random_number = random.randint
+for _ in range(4):
+    pen.forward(100)
+    pen.left(90)
+
+
+
+
+
+turtle.exitonclick()
